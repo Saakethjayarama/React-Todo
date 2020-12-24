08977f979f8f38e31bf112fcbed81251922f51ab
saaketh <saakethaj@gmail.com>
2020-12-24 14:50:29 +0530

    app optimized

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,7 +1,6 @@
 import Header from "../Header";
 import "./App.css";
-import * as R from "react-bootstrap";
-import { Alert, Button } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import TodoForm from "../TodoForm";
 import Todo from "../Todo/Todo";
 
@@ -9,17 +8,17 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <R.Container fluid>
-        <R.Row>
-          <R.Col md={6} className="App__Todos">
+      <Container fluid>
+        <Row>
+          <Col md={6} className="App__Todos">
             <h4>Todos</h4>
             <Todo />
-          </R.Col>
-          <R.Col md={6} className="App__TodoForm">
+          </Col>
+          <Col md={6} className="App__TodoForm">
             <TodoForm />
-          </R.Col>
-        </R.Row>
-      </R.Container>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
