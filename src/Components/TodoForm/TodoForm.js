bb4a3531cdc7eb80c30e6edf6a5fe60de19a5d28
saaketh <saakethaj@gmail.com>
2020-12-24 19:42:46 +0530

    todo form done

--- src/Components/TodoForm/TodoForm.js
+++ src/Components/TodoForm/TodoForm.js
@@ -3,25 +3,50 @@ import "./TodoForm.css";
 // bootstrap
 import { Form, Button } from "react-bootstrap";
 
-function TodoForm() {
+function TodoForm(props) {
+  const state = props.state;
+  const handleChange = props.handleChange;
+  const handleSubmit = props.handleSubmit;
+
   return (
     <div className="TodoForm">
       <div className="TodoForm__title">Todo</div>
       <div className="TodoForm__form">
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <Form.Group controlId="title">
             <Form.Label>Title</Form.Label>
-            <Form.Control type="text" />
+            <Form.Control
+              type="text"
+              name="title"
+              onChange={handleChange}
+              value={state.title}
+            />
           </Form.Group>
           <Form.Group controlId="desc">
             <Form.Label>Description</Form.Label>
-            <Form.Control as="textarea" rows={4} />
+            <Form.Control
+              as="textarea"
+              name="description"
+              rows={4}
+              onChange={handleChange}
+              value={state.description}
+            />
           </Form.Group>
           <Form.Group controlId="dt">
             <Form.Label>Date and Time</Form.Label>
-            <Form.Control type="datetime-local" />
+            <Form.Control
+              type="datetime-local"
+              name="dateTime"
+              onChange={handleChange}
+              value={state.dateTime}
+            />
           </Form.Group>
-          <Button variant="primary" type="submit" className="float-right">
+          <Button
+            variant="primary"
+            type="submit"
+            className="float-right"
+            style={{ marginLeft: "10px" }}
+          >
             Submit
           </Button>
         </Form>
